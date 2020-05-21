350a0cb829d0ffb8e887838ef841d6c0eb75372a
Teodor Szente <teodor98sz@gmail.com>
2020-05-21 13:06:03 +0300

    Solve 2

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/MainActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
